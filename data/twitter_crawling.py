fc35a3e7bdd9dcd2662f6dacc09f9d2ae8b0ad78
Sohyeon Lee <lshhhhh95@gmail.com>
2017-04-06 22:56:26 +0900

    Implement crawling tweets

---
+++ data/twitter_crawling.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+
+import tweepy
+import os
+
+consumer_key = 'MY CONSUMER KEY'
+consumer_secret = 'MY CONSUMER SECRET'
+access_token = 'MY ACCESS TOKEN'
+access_token_secret = 'MY ACCESS TOKEN SECRET'
+
+
+# Request for certification
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+
+# Request for access token
+auth.set_access_token(access_token, access_token_secret)
+
+# Create twitter API
+api = tweepy.API(auth)
+
+# Search by keyword
+keyword = u"봄";
+search = []
+
+cnt = 1
+while (cnt <= 10):
+	tweets = api.search(keyword)
+	for tweet in tweets:
+		search.append(tweet)
+	cnt += 1
+#print(search[0])
+
+wfile1 = open(os.getcwd() + "/crawling_data_keyword.tsv", mode='w')
+data = {}
+for tweet in search:
+	data[u'text'] = tweet.text
+	wfile1.write(data[u'text'].encode('utf-8') + '\n')
+wfile1.close()
+
+
+wfile2 = open(os.getcwd() + "/crawling_data_timeline.tsv", mode='w')
+data = {}
+for tweet in tweepy.Cursor(api.home_timeline).items(10):
+	data[u'text'] = tweet.text
+	wfile2.write(data[u'text'].encode('utf-8') + '\n')
+wfile2.close()
+
+"""
+for tweet in tweepy.Cursor(api.friends).items():
+for tweet in tweepy.Cursor(api.user_timeline).items():
+"""
